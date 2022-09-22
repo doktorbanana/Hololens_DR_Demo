61cb17016b00e973be5761f040bdcbcf362b1ee9
doktorbanana <53558347+doktorbanana@users.noreply.github.com>
2022-09-22 14:25:57 +0200

    added new audio to unity

--- Hololens_Demo/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Hololens_Demo/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,18 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_ARCADE = 989013682U;
+        static const AkUniqueID PLAY_BASS = 1560528335U;
+        static const AkUniqueID PLAY_COFFEE = 1012391550U;
+        static const AkUniqueID PLAY_HH___SNARE = 2625374342U;
+        static const AkUniqueID PLAY_KICK = 711699424U;
+        static const AkUniqueID PLAY_SPEECH = 4122783924U;
+        static const AkUniqueID PLAY_STEMS_CHORDS_L = 3938154147U;
+        static const AkUniqueID PLAY_STEMS_CHORDS_R = 3938154173U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
@@ -21,6 +33,7 @@ namespace AK
     {
         static const AkUniqueID _3D_AUDIO = 4122844158U;
         static const AkUniqueID ARCADE_MACHINE = 2753246078U;
+        static const AkUniqueID BASS = 1291433344U;
         static const AkUniqueID CHORDS_L = 712803560U;
         static const AkUniqueID CHORDS_R = 712803574U;
         static const AkUniqueID COFFEE_MAKER = 2316283105U;
