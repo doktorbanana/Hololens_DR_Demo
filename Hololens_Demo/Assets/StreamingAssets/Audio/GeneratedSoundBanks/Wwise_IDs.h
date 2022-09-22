4100d27095ccbf1d1759e6995adf80862598d2b2
Peter Pompös <pit.martin@web.de>
2022-09-22 12:45:08 +0200

    added new Suorces to Wwise

--- Hololens_Demo/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Hololens_Demo/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,13 +11,6 @@
 
 namespace AK
 {
-    namespace EVENTS
-    {
-        static const AkUniqueID PLAY_ARCADE = 989013682U;
-        static const AkUniqueID PLAY_COFFEE = 1012391550U;
-        static const AkUniqueID PLAY_SPEECH = 4122783924U;
-    } // namespace EVENTS
-
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
@@ -28,7 +21,11 @@ namespace AK
     {
         static const AkUniqueID _3D_AUDIO = 4122844158U;
         static const AkUniqueID ARCADE_MACHINE = 2753246078U;
+        static const AkUniqueID CHORDS_L = 712803560U;
+        static const AkUniqueID CHORDS_R = 712803574U;
         static const AkUniqueID COFFEE_MAKER = 2316283105U;
+        static const AkUniqueID HATS_AND_SNARE = 3939317979U;
+        static const AkUniqueID KICK_DRUM = 1553909153U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID SPEECH = 4245458931U;
     } // namespace BUSSES
